e83f4ff52bf5f9e1757879b8d3fa5bcfe4289561
bouadila <firas.bouadila@esprit.tn>
2022-05-11 00:36:07 +0100

    Update cors options

--- index.js
+++ index.js
@@ -28,19 +28,9 @@ const whitelist = process.env.WHITELISTED_DOMAINS
   ? process.env.WHITELISTED_DOMAINS.split(",")
   : [];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
 
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.static("images"));
 app.use("/uploads", express.static(path.join("uploads")));
